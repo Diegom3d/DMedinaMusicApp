f7a46990b0fc07e7d604d9c37b51b719db6fda47
Diegom3d <Diegom3d@users.noreply.github.com>
2025-10-16 09:57:52 -0600

    Correcion AlbumRepository.kt, por duplicado de clase en el viewmodel

--- app/src/main/java/com/example/dmedinamusicapp/data/AlbumRepository.kt
+++ app/src/main/java/com/example/dmedinamusicapp/data/AlbumRepository.kt
@@ -1,37 +1,11 @@
-package com.example.dmedinamusicapp.viewmodel
+package com.example.dmedinamusicapp.data
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.dmedinamusicapp.data.Album
-import com.example.dmedinamusicapp.data.RetrofitInstance
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-
-class AlbumViewModel : ViewModel() {
-    private val _albums = MutableStateFlow<List<Album>>(emptyList())
-    val albums: StateFlow<List<Album>> = _albums
-
-    private val _selectedAlbum = MutableStateFlow<Album?>(null)
-    val selectedAlbum: StateFlow<Album?> = _selectedAlbum
-
-    init {
-        viewModelScope.launch {
-            try {
-                _albums.value = RetrofitInstance.api.getAlbums()
-            } catch (e: Exception) {
-
-            }
-        }
+class AlbumRepository {
+    suspend fun fetchAlbums(): List<Album> {
+        return RetrofitInstance.api.getAlbums()
     }
 
-    fun loadAlbumById(id: String) {
-        viewModelScope.launch {
-            try {
-                _selectedAlbum.value = RetrofitInstance.api.getAlbumById(id)
-            } catch (e: Exception) {
-
-            }
-        }
+    suspend fun fetchAlbumById(id: String): Album {
+        return RetrofitInstance.api.getAlbumById(id)
     }
 }
