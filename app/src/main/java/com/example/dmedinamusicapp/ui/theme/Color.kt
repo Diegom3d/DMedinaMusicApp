97e8a3641ed3b3f7daaa502a759b8c69e44350e8
Diegom3d <Diegom3d@users.noreply.github.com>
2025-10-16 20:51:48 -0600

    Carpeta de ui para la interfaz

---
+++ app/src/main/java/com/example/dmedinamusicapp/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.dmedinamusicapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF361A49)
+val PurpleGrey40 = Color(0xFFBBB3CB)
+val DarkBackground = Color(0xFF100F0F)
+val LightCard = Color(0xFFFFFFFF)
+val ScrimPurple = Color(0x8008081E)
---
+++ app/src/main/java/com/example/dmedinamusicapp/ui/theme/Theme.kt
@@ -0,0 +1,28 @@
+package com.example.dmedinamusicapp.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey40,
+    background = Color.White,
+    surface = LightCard,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black
+)
+
+@Composable
+fun FakeMusicAppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = LightColors,
+        typography = AppTypography,
+        shapes = AppShapes,
+        content = content
+    )
+}
